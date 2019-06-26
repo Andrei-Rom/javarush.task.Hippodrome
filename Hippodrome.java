92e93a588aa130021e9510342db2cfd875ce2985
Andrei Rom <andrisrom@gmail.com>
2019-06-26 11:41:52 +0300

    task(6)

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,12 +1,22 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
-    public static void main(String[] args) {
-
-    }
     private List<Horse> horses;
+    static Hippodrome game;
+
+    public static void main(String[] args) {
+        game = new Hippodrome(new ArrayList<>());
+        Horse recco = new Horse("Recco", 3d, 0d);
+        Horse racket = new Horse("Racket", 3d, 0d);
+        Horse sharif = new Horse("Sharif", 3d,0d);
+        game.getHorses().add(recco);
+        game.getHorses().add(racket);
+        game.getHorses().add(sharif);
+        
+    }
 
     public List<Horse> getHorses() {
         return horses;
@@ -15,4 +25,6 @@ public class Hippodrome {
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
     }
+
+
 }
