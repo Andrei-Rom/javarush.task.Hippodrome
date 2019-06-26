19df4245247d59f8e38d096123581fbacf04e0f6
Andrei Rom <andrisrom@gmail.com>
2019-06-26 11:24:31 +0300

    second task

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,7 +1,14 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.List;
+
 public class Hippodrome {
     public static void main(String[] args) {
 
+    }
+    private List<Horse> horses;
+
+    public List<Horse> getHorses() {
+        return horses;
     }
 }
